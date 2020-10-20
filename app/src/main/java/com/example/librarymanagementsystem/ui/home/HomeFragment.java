0599a3211625d2c9e041903cfe56ae396a6ae499
kavyanair007 <kavyakottarath@gmail.com>
2020-10-20 16:33:18 +0530

    :books: chore: add Homepage
    Title and description of the app

--- app/src/main/java/com/example/librarymanagementsystem/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/librarymanagementsystem/ui/home/HomeFragment.java
@@ -23,11 +23,11 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        //final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //textView.setText(s);
             }
         });
         return root;
